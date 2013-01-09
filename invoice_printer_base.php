cf125d58e18e95ab859a23000fe5f5344936485f
Sami Lempinen <sami@lempinen.net>
2013-01-09 13:15:01 +0200

    PDF invoice beautification.

--- invoice_printer_base.php
+++ invoice_printer_base.php
@@ -311,7 +311,8 @@ abstract class InvoicePrinterBase
       $locStr = 'Invoice';
     
     // Invoice info headers
-    $pdf->SetXY(115,10);
+    $info_x = 129;
+    $pdf->SetXY($info_x, 10);
     $pdf->SetFont('Helvetica','B',12);
     if ($this->printStyle == 'dispatch')
       $pdf->Cell(40, 5, $GLOBALS['locPDFDispatchNoteHeader'], 0, 1, 'R');
@@ -322,9 +323,9 @@ abstract class InvoicePrinterBase
     elseif ($invoiceData['state_id'] == 6)
       $pdf->Cell(40, 5, $GLOBALS['locPDFSecondReminderHeader'], 0, 1, 'R');
     else
-      $pdf->Cell(40, 5, $GLOBALS['locPDFInvoiceHeader'], 0, 1, 'R');
+      $pdf->Cell(68, 5, $GLOBALS['locPDFInvoiceHeader'], 0, 1, 'R');
     $pdf->SetFont('Helvetica','',10);
-    $pdf->SetXY(115, $pdf->GetY()+5);
+    $pdf->SetXY($info_x, $pdf->GetY()+5);
     if ($recipientData['customer_no'] != 0)
     {
       $pdf->Cell(40, 5, $GLOBALS['locPDFCustomerNumber'] . ': ', 0, 0, 'R');
@@ -332,52 +333,52 @@ abstract class InvoicePrinterBase
     }
     if ($recipientData['company_id'])
     {
-      $pdf->SetX(115);
+      $pdf->SetX($info_x);
       $pdf->Cell(40, 5, $GLOBALS['locPDFClientVATID'] . ': ', 0, 0, 'R');
-      $pdf->Cell(60, 5, $recipientData['company_id'], 0, 1);
+      $pdf->Cell(28, 5, $recipientData['company_id'], 0, 1, 'R');
     }
-    $pdf->SetX(115);
+    $pdf->SetX($info_x);
     $pdf->Cell(40, 5, $GLOBALS["locPDF${locStr}Number"] . ': ', 0, 0, 'R');
-    $pdf->Cell(60, 5, $invoiceData['invoice_no'], 0, 1);
-    $pdf->SetX(115);
+    $pdf->Cell(28, 5, $invoiceData['invoice_no'], 0, 1, 'R');
+    $pdf->SetX($info_x);
     $pdf->Cell(40, 5, $GLOBALS["locPDF${locStr}Date"] . ': ', 0, 0, 'R');
     $strInvoiceDate = $this->_formatDate($invoiceData['invoice_date']);
     $strDueDate = $this->_formatDate($invoiceData['due_date']);
-    $pdf->Cell(60, 5, $strInvoiceDate, 0, 1);
+    $pdf->Cell(28, 5, $strInvoiceDate, 0, 1, 'R');
     if ($this->printStyle == 'invoice')
     {
-      $pdf->SetX(115);
+      $pdf->SetX($info_x);
       $pdf->Cell(40, 5, $GLOBALS['locPDFDueDate'] . ': ', 0, 0, 'R');
-      $pdf->Cell(60, 5, $strDueDate, 0, 1);
-      $pdf->SetX(115);
+      $pdf->Cell(28, 5, $strDueDate, 0, 1, 'R');
+      $pdf->SetX($info_x);
       $pdf->Cell(40, 5, $GLOBALS['locPDFTermsOfPayment'] . ': ', 0, 0, 'R');
       $paymentDays = round(dbDate2UnixTime($invoiceData['due_date']) / 3600 / 24 - dbDate2UnixTime($invoiceData['invoice_date']) / 3600 / 24);
       if ($paymentDays < 0) //weird
         $paymentDays = getSetting('invoice_payment_days');
-      $pdf->Cell(60, 5, sprintf(getSetting('invoice_terms_of_payment'), $paymentDays), 0, 1);
-      $pdf->SetX(115);
+      $pdf->Cell(28, 5, sprintf(getSetting('invoice_terms_of_payment'), $paymentDays), 0, 1, 'R');
+      $pdf->SetX($info_x);
       $pdf->Cell(40, 5, $GLOBALS['locPDFPeriodForComplaints'] . ': ', 0, 0, 'R');
-      $pdf->Cell(60, 5, getSetting('invoice_period_for_complaints'), 0, 1);
-      $pdf->SetX(115);
+      $pdf->Cell(28, 5, getSetting('invoice_period_for_complaints'), 0, 1, 'R');
+      $pdf->SetX($info_x);
       $pdf->Cell(40, 5, $GLOBALS['locPDFPenaltyInterest'] . ': ', 0, 0, 'R');
-      $pdf->Cell(60, 5, $this->_formatNumber(getSetting('invoice_penalty_interest'), 1, true) . ' %', 0, 1);
-      $pdf->SetX(115);
+      $pdf->Cell(28, 5, $this->_formatNumber(getSetting('invoice_penalty_interest'), 1, true) . ' %', 0, 1, 'R');
+      $pdf->SetX($info_x);
       if ($this->refNumber)
       {
         $pdf->Cell(40, 5, $GLOBALS['locPDFInvoiceRefNr'] . ': ', 0, 0, 'R');
-        $pdf->Cell(60, 5, $this->refNumber, 0, 1);
+        $pdf->Cell(28, 5, $this->refNumber, 0, 1, 'R');
       }
     }
     
     if ($invoiceData['reference'] && $this->printStyle != 'dispatch')
     {
-      $pdf->SetX(115);
+      $pdf->SetX($info_x);
       $pdf->Cell(40, 5, $GLOBALS['locPDFYourReference'] . ': ', 0, 0, 'R');
-      $pdf->Cell(60, 5, $invoiceData['reference'], 0, 1);
+      $pdf->Cell(28, 5, $invoiceData['reference'], 0, 1, 'R');
     }
     if (isset($invoiceData['info']) && $invoiceData['info'])
     {
-      $pdf->SetX(115);
+      $pdf->SetX($info_x);
       $pdf->Cell(40, 5, $GLOBALS['locPDFAdditionalInformation'] . ': ', 0, 0, 'R');
       $pdf->MultiCell(50, 5, $invoiceData['info'], 0, 'L', 0);
     }
@@ -437,7 +438,7 @@ abstract class InvoicePrinterBase
       $pdf->SetXY(4, $pdf->GetY());
       $pdf->Cell(80, 5, $GLOBALS['locPDFInvoiceStatement'], 0, 0, 'L');
       $pdf->SetFont('Helvetica','',10);
-      $pdf->SetX(115);
+      $pdf->SetX($info_x);
       
       if ($this->printStyle == 'dispatch')
         $locStr = 'DispatchNote';
